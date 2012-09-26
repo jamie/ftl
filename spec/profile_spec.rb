5db0cd13e7f5470a753648b04452fd766497be4f
Jamie Macey <jamie@tracefunc.com>
2012-09-26 14:01:10 -0700

    clarify build style

--- spec/profile_spec.rb
+++ spec/profile_spec.rb
@@ -7,15 +7,24 @@ describe Profile do
   }
   
   it "loads version" do
-    profile['version'].should == 4
+    profile.version.should == 4
   end
 
   it "loads achievements" do
-    profile['achievements'].should == [
+    profile.achievements.should == [
       ['ACH_SECTOR_5', 'easy'],
       ['ACH_UNLOCK_ALL', 'normal'],
       ['ACH_FULL_ARSENAL', 'easy'],
       ['ACH_TOUGH_SHIP', 'easy']
     ]
+  end
+
+  it "builds with a hash" do
+    p = Profile.new(
+      :version => 4,
+      :achievements => [['ACH_SECTOR_5', 'easy']]
+    )
+    p.version.should == 4
+    p.achievements.should == [['ACH_SECTOR_5', 'easy']]
   end
 end
