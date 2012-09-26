59a7f0f419528cf956339f6b863a887d2f1663e8
Jamie Macey <jamie@tracefunc.com>
2012-09-26 13:38:53 -0700

    inline save file for specs

--- spec/profile_spec.rb
+++ spec/profile_spec.rb
@@ -2,8 +2,7 @@ require 'spec_helper'
 
 describe Profile do
   let(:profile) {
-    file = File.expand_path(YAML.load(File.read('config.yml'))['mac']) + '/prof.sav'
-    file = File.open(file)
+    file = File.open('./spec/prof.sav')
     Profile.read(file)
   }
   
